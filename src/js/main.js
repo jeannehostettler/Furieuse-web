49ef7f4306a1afa0bd7ed7cf815ad677cd5f74d4
jeannehostettler <122967561+jeannehostettler@users.noreply.github.com>
2024-11-14 16:40:37 +0100

    ajout du fondu rouge sur le parallax

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,14 @@ gsap.utils.toArray(".parallax").forEach((layer) => {
   const movement = -(layer.offsetHeight * depth);
   tl.to(layer, { y: movement, ease: "none" }, 0);
 });
+
+const contents = gsap.utils.toArray("#horizontal .content");
+
+gsap.to(contents, {
+  xPercent: -100 * (contents.length - 1), //deplacement de l'élément
+  scrollTrigger: {
+    trigger: "#horizontal",
+    pin: true, //épingle l'élément a sa position actuelle
+    scrub: 1, //la valeur définir un delais à l'animation
+  },
+});
